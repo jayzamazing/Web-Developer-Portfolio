18f99d90426fb379a999bef928ac476ba350d53d
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-06-19 14:15:50 -0400

    Added jquery-validation to bower.json and to index.html. Deleted the validation I was going to use in favor of using validation in app.js, added email, message, and name rules along with messages for those rules. Minor form changes in index.html.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,31 @@
 $(document).ready(function() {
-  $('#contactForm').submit(function(event) {
-    event.preventDefault();
-
-  });
-  var validators = {
-    name: {
-      regex: /^[A-Za-z]{3,}$/
+  //validate using jquery validation
+  $('#contact-form').validate({
+    rules: {//rules for validation
+      full_name: {
+        minlength: 2,//set name as required and allowed for nicknames
+        required: true
+      },
+      email: {//set to look for valid email addresses and required
+        email: true,
+        required: true
+      },
+      message: {//set as required
+        required: true
+      }
     },
-    email: {
-      regex: /\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/
+    messages: {
+      full_name: {//messages to show if field is empty or if the minimum length is not meet
+        required: "I need to know who is trying to contact me",
+        minlength: jQuery.validator.format("Name must be at least {0} characters long.")
+      },
+      email: {//messages to show if no email address is set or if email address is invalid
+        required: "Please enter a valid email address so I can get back in touch with you.",
+        email: "Please enter a valid email address so I can get back in touch with you."
+      },
+      message: {//message to show if message field is empty
+        required: "Please leave me a description of how I can help you."
+      }
     }
-  };
+  });
 });
